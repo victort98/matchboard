58d7d423878b062e0fadcb9a74d669ae3c5dcd2d
mriduava <mriduava@gmail.com>
2020-04-22 10:43:17 +0200

    add websocet with spring boot

---
+++ src/main/java/com/socketsexample/mysockets/MysocketsApplication.java
@@ -0,0 +1,13 @@
+package com.socketsexample.mysockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysocketsApplication.class, args);
+	}
+
+}

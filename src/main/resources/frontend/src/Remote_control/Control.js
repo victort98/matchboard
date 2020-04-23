6be20d6856f04ce8f80882d3c7fcd727b9025550
Victor <Victortornbom@gmail.com>
2020-04-23 13:43:07 +0200

    very basic structure of the remote control

--- src/main/resources/frontend/src/Remote_control/Control.js
+++ src/main/resources/frontend/src/Remote_control/Control.js
@@ -2,20 +2,71 @@ import React from 'react';
 
 function Control() {
     return(
-        <div className="Control_container">
-            <button className="start_clock">START CLOCK</button>
-            <button className="stop_clock">STOP CLOCK</button>
-            <button className="reset">RESET</button>
-            <br />
-            <div className="clock">90:00</div>
+        <div>
+            <div className="clockButtons">
+                <button className="start">START CLOCK</button>
+                <button className="stop">STOP CLOCK</button>
+                <button className="reset">RESET</button>
+            </div>
+        <div className="controlContainer">
             <div className="team1">
-                <input placeholder="Namn för lag 1"></input>
+                <p>SET TEAM 1</p>
+                <input className="teamName" placeholder="Name of team 1" type="text"></input>
+                <br />
+                <p>SET SCORE</p>
+                <input className="team1Score" type="number" min="0"></input>
+                <div className="team1Information">
+                <p>YELLOW CARD</p><p>RED CARD</p>
+                <input type="number"></input>
+                <input type="number"></input>
+                <p>CORNERS</p><p>OFFSIDES</p>
+                <input type="number"></input>
+                <input type="number"></input>
+                <p>SHOTS</p><p>FOULS</p>
+                <input type="number"></input>
+                <input type="number"></input>
+                <p>SHOTS ON TARGET</p>
+                <input type="number"></input>
+                </div>
             </div>
 
+
+
+            <div className="middleInformation">
+                <div className="clock">90:00</div>
+                <br />
+                <input type="number"></input>
+                <input type="number"></input>
+                <br />
+                <button>SET EXTRA TIME</button>
+                <p>SELECT SREEN TO CAST</p>
+                <input placeholder="SCOREBOARD" type="text"></input>
+                <br />
+                <button>BROADCAST</button>
+            </div>
+
+            
             <div className="team2">
-                <input placeholder="Namn för lag 2"></input>
+            <p>SET TEAM 1</p>
+                <input className="teamName" placeholder="Name of team 2" type="text"></input>
+                <br />
+                <p>SET SCORE</p>
+                <input className="team2Score" type="number" min="0"></input>
+                <div className="team2Information">
+                <p>YELLOW CARD</p><p>RED CARD</p>
+                <input type="number"></input>
+                <input type="number"></input>
+                <p>CORNERS</p><p>OFFSIDES</p>
+                <input type="number"></input>
+                <input type="number"></input>
+                <p>SHOTS</p><p>FOULS</p>
+                <input type="number"></input>
+                <input type="number"></input>
+                <p>SHOTS ON TARGET</p>
+                <input type="number"></input>
+                </div>
             </div>
-
+        </div>
         </div>
     )
 }

7b873e3f9bae055f911a96667635c2d18f08c17b
Victor <Victortornbom@gmail.com>
2020-04-24 10:05:30 +0200

    styling and more structure to the page

--- src/main/resources/frontend/src/Remote_control/Control.js
+++ src/main/resources/frontend/src/Remote_control/Control.js
@@ -2,41 +2,45 @@ import React from 'react';
 
 function Control() {
     return(
-        <div>
-            <div className="clockButtons">
-                <button className="start">START CLOCK</button>
-                <button className="stop">STOP CLOCK</button>
-                <button className="reset">RESET</button>
-            </div>
-        <div className="controlContainer">
+    <div>
+        <div className="clockButtons">
+            <button className="start btnStyle">START CLOCK</button>
+            <button className="stop btnStyle">STOP CLOCK</button>
+            <button className="reset btnStyle">RESET</button>
+        </div>
+
+            <div className="controlContainer">
             <div className="team1">
-                <p>SET TEAM 1</p>
+                <label>SET TEAM 1</label>
+                <br />
                 <input className="teamName" placeholder="Name of team 1" type="text"></input>
                 <br />
-                <p>SET SCORE</p>
-                <input className="team1Score" type="number" min="0"></input>
+                <label>SET SCORE</label>
+                <br/>
+                <input className="teamScore" type="number" min="0" placeholder="0"></input>
                 <div className="team1Information">
-                <p>YELLOW CARD</p><p>RED CARD</p>
-                <input type="number"></input>
-                <input type="number"></input>
+                <p>YELLOW CARD</p>
+                <p>RED CARD</p>
+                <input className="inputStyling" type="number" min="0" placeholder="0"></input>
+                <input className="inputStyling" type="number" min="0" placeholder="0"></input>
                 <p>CORNERS</p><p>OFFSIDES</p>
-                <input type="number"></input>
-                <input type="number"></input>
+                <input className="inputStyling" type="number" min="0" placeholder="0"></input>
+                <input className="inputStyling" type="number" min="0" placeholder="0"></input>
                 <p>SHOTS</p><p>FOULS</p>
-                <input type="number"></input>
-                <input type="number"></input>
+                <input className="inputStyling" type="number" min="0" placeholder="0"></input>
+                <input className="inputStyling" type="number" min="0" placeholder="0"></input>
                 <p>SHOTS ON TARGET</p>
-                <input type="number"></input>
+                <input className="inputStyling" type="number" min="0" placeholder="0"></input>
                 </div>
             </div>
-
-
 
             <div className="middleInformation">
                 <div className="clock">90:00</div>
                 <br />
-                <input type="number"></input>
-                <input type="number"></input>
+                <div className="overtime">
+                <input className="middleInputStyling" type="number" min="0" placeholder="0"></input>
+                <input className="middleInputStyling" type="number" min="0" placeholder="0"></input>
+                </div>
                 <br />
                 <button>SET EXTRA TIME</button>
                 <p>SELECT SREEN TO CAST</p>
@@ -45,29 +49,29 @@ function Control() {
                 <button>BROADCAST</button>
             </div>
 
-            
             <div className="team2">
-            <p>SET TEAM 1</p>
-                <input className="teamName" placeholder="Name of team 2" type="text"></input>
+                <label>SET TEAM 1</label>
                 <br />
-                <p>SET SCORE</p>
-                <input className="team2Score" type="number" min="0"></input>
+                    <input className="teamName" placeholder="Name of team 2" type="text"></input>
+                    <br />
+                    <p>SET SCORE</p>
+                    <input className="teamScore" type="number" min="0" placeholder="0"></input>
                 <div className="team2Information">
-                <p>YELLOW CARD</p><p>RED CARD</p>
-                <input type="number"></input>
-                <input type="number"></input>
-                <p>CORNERS</p><p>OFFSIDES</p>
-                <input type="number"></input>
-                <input type="number"></input>
-                <p>SHOTS</p><p>FOULS</p>
-                <input type="number"></input>
-                <input type="number"></input>
-                <p>SHOTS ON TARGET</p>
-                <input type="number"></input>
+                    <p>YELLOW CARD</p><p>RED CARD</p>
+                    <input className="inputStyling" type="number" min="0" placeholder="0"></input>
+                    <input className="inputStyling" type="number" min="0" placeholder="0"></input>
+                    <p>CORNERS</p><p>OFFSIDES</p>
+                    <input className="inputStyling" type="number" min="0" placeholder="0"></input>
+                    <input className="inputStyling" type="number" min="0" placeholder="0"></input>
+                    <p>SHOTS</p><p>FOULS</p>
+                    <input className="inputStyling" type="number" min="0" placeholder="0"></input>
+                    <input className="inputStyling" type="number" min="0" placeholder="0"></input>
+                    <p>SHOTS ON TARGET</p>
+                    <input className="inputStyling" type="number" min="0" placeholder="0"></input>
                 </div>
             </div>
         </div>
-        </div>
+    </div>
     )
 }
 

9627b2772f41e0db0e6198a88ce5cb5ff9df29e7
mriduava <mriduava@gmail.com>
2020-05-31 23:15:38 +0200

    add admin and operator panels

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import MatchBoard from './matchboard/MatchBoard.js'
+import MatchBoard from './matchboard/MatchBoard'
 import ControlBoardContextProvider from './contexts/ControlBoardContextProvider'
 import ScoreBoardContextProvider from './contexts/ScoreBoardContextProvider'
 import ScoreClockContextProvider from './contexts/ScoreClockContextProvider'
@@ -10,17 +10,17 @@ import UserContextProvider from './contexts/UserContextProvider'
 import Playerslist from './themes/football/Playerslist2'
 import Statistics from './themes/football/Statistics2'
 import MasterClock from './controlboard/MasterClock'
-import Basketball from './themes/basketball/Basketball.js'
-import Hockey from './themes/hockey/Hockey.js'
-import UserLogin from './users/UserLogin.js'
+import Basketball from './themes/basketball/Basketball'
+import Hockey from './themes/hockey/Hockey'
+import UserLogin from './users/UserLogin'
 import Register from './users/UserRegister'
+import AdminPanel from './users/AdminPanel'
+import OperatorPanel from './users/OperatorPanel'
 import ProtectedRoutes from './routes/ProtectedRoutes'
 import LeagueTable from './controlboard/LeagueTable'
 import FootballFixtures from './themes/football/Fixtures'
 import PointTable from './themes/football/PointTable'
 import MatchFixture from './controlboard/MatchFixtures'
-
-
 
 const App = () => {
   return (
@@ -34,6 +34,8 @@ const App = () => {
                   <ProtectedRoutes/> 
                   <Route exact path='/' component={UserLogin}/>
                   <Route exact path='/register' component={Register}/>   
+                  <Route exact path='/admin' component={AdminPanel}/>   
+                  <Route exact path='/operator' component={OperatorPanel}/>   
                   <Route exact path='/matchboard' component={MatchBoard}/>            
                   <Route exact path='/playerslist' component={Playerslist}/>            
                   <Route exact path='/statistics' component={Statistics}/>            
